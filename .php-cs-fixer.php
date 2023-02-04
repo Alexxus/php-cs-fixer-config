3a6ba0b1d6e7b1f254a8df15c9e570f8b215f878
Alexxus <alexxusdev@gmail.com>
2023-02-04 13:32:04 +0300

    Added some rules, mostely based on Laravel and Symphony preferences

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,10 +6,39 @@ use PhpCsFixer\Finder;
 $rules = [
     '@PSR12' => true,
     'array_indentation' => true,
-    'single_quote' => true,
+    'blank_line_before_statement' => [
+        'statements' => ['return'],
+    ],
     'braces' => [
         'position_after_control_structures' => 'next',
-    ]
+    ],
+    'cast_spaces' => true,
+    'fully_qualified_strict_types' => true,
+    'function_typehint_space' => true,
+    'linebreak_after_opening_tag' => true,
+    'magic_constant_casing' => true,
+    'no_empty_statement' => true,
+    'no_leading_namespace_whitespace' => true,
+    'no_mixed_echo_print' => [
+        'use' => 'echo',
+    ],
+    'no_multiline_whitespace_around_double_arrow' => true,
+    'no_short_bool_cast' => true,
+    'no_singleline_whitespace_before_semicolons' => true,
+    'no_spaces_around_offset' => [
+        'positions' => ['inside', 'outside'],
+    ],
+    'no_useless_return' => true,
+    'no_whitespace_before_comma_in_array' => true,
+    'normalize_index_brace' => true,
+    'single_quote' => true,
+    'standardize_not_equals' => true,
+    'trailing_comma_in_multiline' => [
+        'elements' => ['arrays'],
+    ],
+    'trim_array_spaces' => true,
+    'unary_operator_spaces' => true,
+    'whitespace_after_comma_in_array' => true,
 ];
 
 $finder =   Finder::create()
